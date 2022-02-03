b7cf109606b29d86756d24491b2480d8d3ff462d
Sahil-4 <www.sahil.875020@gmail.com>
2022-02-03 13:31:16 +0530

    Day 8 completed

--- Day-6-Lets-Review/index.js
+++ Day-6-Lets-Review/index.js
@@ -1,6 +1,28 @@
 function processData(input) {
     //Enter your code here
-} 
+    // console.log(input.split("\n"));
+    let test_cases = input.split("\n")[0];
+    const words = [];
+
+    for (let i = 1; i < input.split("\n").length; i++) {
+        words.push(input.split("\n")[i]);
+    }
+
+    words.forEach((w) => {
+        let even_chars = "", odd_chars = "";
+        let char_arr = w.split("");
+
+        for (let i = 0; i < char_arr.length; i++) {
+            if (i % 2 == 0) {
+                even_chars += char_arr[i];
+            } else {
+                odd_chars += char_arr[i];
+            }
+        }
+        
+        console.log(even_chars, odd_chars);
+    })
+}
 
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
@@ -10,5 +32,5 @@ process.stdin.on("data", function (input) {
 });
 
 process.stdin.on("end", function () {
-   processData(_input);
+    processData(_input);
 });
